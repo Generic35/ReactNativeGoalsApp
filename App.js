bf8a89f45609854cf557fe94c822641e4500ba11
Eric L <ricklamontagne@gmail.com>
2022-12-19 15:05:28 +0300

    [feat] Add the abilty to see where the list, input and button with be located on the screen

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Hello Eric From react 2</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' />
+        <Button title='Add Goal' />
       </View>
-      <Text style={styles.dummyText}>Hello World2!</Text>
-      <Button title={'Tap me!'} />
+      <View>
+        <Text>List of Goals</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: 'blue'
+const styles = StyleSheet.create(
+  {
+    appContainer: {
+      padding: 50,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
+    textInput: {
+      borderWidth: 1,
+      borderColor: '#CCCCCC',
+      width: '80%',
+      marginRight: 8,
+      padding: 8,
+    }
   }
-});
+);
