ad13789e94709981461b5634b3eab4d05985ffad
Eric L <ricklamontagne@gmail.com>
2022-12-28 10:49:04 +0300

    [feat] Add the ability for a user to scroll through the list by using FlatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, StyleSheet, TextInput, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, Text, View, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -11,7 +11,7 @@ export default function App() {
   }
 
   function addGoalHandler() {
-    setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText])
+    setCourseGoals(currentCourseGoals => [...currentCourseGoals, { text: enteredGoalText, id: Math.random().toString() }])
   }
 
   return (
@@ -21,11 +21,15 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) =>
-          <View style={styles.goalItem} key={goal}>
-            <Text style={styles.goalText}>{goal}</Text>
-          </View>
-        )}
+        <FlatList
+          alwaysBounceVertical={false}
+          data={courseGoals} renderItem={(itemData) => {
+            return (<View style={styles.goalItem}>
+              <Text style={styles.goalText}>{itemData.item.text}</Text>
+            </View>);
+          }}
+          keyExtractor={(item, index) => { return item.id }}
+        />
       </View>
     </View>
   );
